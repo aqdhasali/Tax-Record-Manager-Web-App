cccda376c3c5070bdf5cb7d8da4c1d9545d0e771
Aqdhas Ali <aqdhasa@verdentra.com>
2025-09-03 20:04:10 +0530

    Added Docker Compose Files --INCOMPLETE

--- TaxRecordsFrontend/src/app/app.routes.ts
+++ TaxRecordsFrontend/src/app/app.routes.ts
@@ -3,8 +3,21 @@ import { TaxRecordList } from './components/tax-record-list/tax-record-list';
 import { TaxRecordForm } from './components/tax-record-form/tax-record-form';
 
 export const routes: Routes = [
-    { path: '', component: TaxRecordList},
-    { path: 'new', component: TaxRecordForm},
-    { path: 'edit/:id', component:TaxRecordForm},
-    { path: '**', redirectTo:''}
+    { 
+        path: '', 
+        component: TaxRecordList
+    },
+    { 
+        path: 'new', 
+        component: TaxRecordForm
+    },
+    { 
+        path: 'edit/:id', 
+        component: TaxRecordForm,
+        data: { prerender: false }
+    },
+    { 
+        path: '**', 
+        redirectTo:''
+    }
 ];
---
+++ TaxRecordsFrontend/src/environment/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://api:5056/api'
+};
